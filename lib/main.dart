f09c06bac082f24159ede128c996826c17a1f90d
Julian Garzon <bryangarzon22@gmail.com>
2020-04-10 14:22:29 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Tracker'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(
+              'Hola mundo'
+          ),
+        ],
+      ),
+    );
+  }
+}
