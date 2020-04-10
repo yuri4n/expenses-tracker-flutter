2480735ba1fa81005bb9e84d2eab5f7c0b67ab98
Julian Garzon <bryangarzon22@gmail.com>
2020-04-10 16:43:49 -0500

    Transactions list mapped on semi-styled rows

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -11,16 +13,76 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1', title: 'New Shoes', amount: 54.99, date: new DateTime.now(),),
+    Transaction(
+      id: 't2', title: 'Groceries', amount: 80.87, date: new DateTime.now(),),
+    Transaction(
+      id: 't3', title: 'Bills', amount: 22.43, date: new DateTime.now(),),
+    Transaction(
+      id: 't4',
+      title: 'Mac reparation',
+      amount: 200.00,
+      date: new DateTime.now(),),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.orangeAccent,
         title: Text('Expense Tracker'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
-          Text(
-              'Hola mundo'
+          Container(
+            width: double.infinity,
+            child: Card(
+              elevation: 5,
+              child: Text('Charts'),
+            ),
+          ),
+          Column(
+            children: transactions.map((transaction) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 20
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.green, width: 2),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        transaction.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          transaction.title,
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text(transaction.date.toString()),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
           ),
         ],
       ),
