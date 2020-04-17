6d47fd6f171d34c434b8196ef2a9921835904586
Julian Garzon <bryangarzon22@gmail.com>
2020-04-17 00:20:34 -0500

    Adaptive flat button

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -4,6 +4,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+import './adaptive_flat_button.dart';
+
 class NewTransaction extends StatefulWidget {
   final Function addTransaction;
 
@@ -81,16 +83,7 @@ class _NewTransactionState extends State<NewTransaction> {
                           ? 'No date Chosen'
                           : 'Picked date: ${DateFormat.yMd().format(_selectedDate)}'),
                     ),
-                    Platform.isIOS
-                        ? CupertinoButton(
-                            child: Text('Choose date'),
-                            onPressed: _presentDatePicker,
-                          )
-                        : FlatButton(
-                            textColor: Theme.of(context).primaryColor,
-                            child: Text('Choose date'),
-                            onPressed: _presentDatePicker,
-                          ),
+                    AdaptiveFlatButton('Choose Date', _presentDatePicker),
                   ],
                 ),
               ),
